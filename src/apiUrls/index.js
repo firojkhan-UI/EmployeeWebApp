f917e0377dbcc23dbf56d0675078a187dc27db06
firoj khan <https://github.com/firojkhan-UI>
2024-08-06 00:07:57 +0530

    list employee api integ fixed

--- src/apiUrls/index.js
+++ src/apiUrls/index.js
@@ -1,4 +1,5 @@
 export const CREATE_EMP_URL = "https://dummy.restapiexample.com/api/v1/create";
 export const LIST_EMP_URL = "https://dummy.restapiexample.com/api/v1/employees";
+// export const LIST_EMP_URL = "https://hub.dummyapis.com/employee?noofRecords=10&idStarts=1001"
 export const DELETE_EMP_URL = "https://dummy.restapiexample.com/api/v1/delete/2";
 export const UPDATE_EMP_URL = "https://dummy.restapiexample.com/api/v1/update/21"
