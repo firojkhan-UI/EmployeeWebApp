7b664b3d395d9ef00f315c72f5ad67d0946bb7ae
Firoj <firoj@Firojs-MacBook-Pro.local>
2024-08-06 23:20:06 +0530

    added form data

---
+++ src/common-components/FormData.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const FormData = () => {
+  return <div style={{width: '400px', background: '#F#F#F',height: '400px', borderRadius: '8px', border: '1px solid'}}>
+    <form>
+        <label>Employee Name</label>
+        <input />
+        <label>Employee Salary</label>
+        <input />
+        <label>Employee age</label>
+        <input />
+        <label>ID</label>
+        <input />
+    </form>
+  </div>;
+};
+
+export default FormData;
